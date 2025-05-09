f6a6a67a76cb2b39790bbf1a9e42f7ad0e38b71c
GreenRoc114 <168276820+GreenRoc114@users.noreply.github.com>
2025-05-09 22:06:12 +0800

    Update blog.config.ts

--- blog.config.ts
+++ blog.config.ts
@@ -8,27 +8,26 @@ export { zhCN as dateLocale } from 'date-fns/locale/zh-CN'
 // 存储 nuxt.config 和 app.config 共用的配置
 // 此处为启动时需要的配置，启动后可变配置位于 app/app.config.ts
 const blogConfig = {
-    title: '纸鹿摸鱼处',
-    subtitle: '纸鹿至麓不知路，支炉制露不止漉',
+    title: 'GreenRoc的博客',
+    subtitle: 'No Fate, We Make.',
     // 长 description 利好于 SEO
-    description: '纸鹿本鹿的个人博客，分享技术与生活。“折腾不止，摸鱼生活——摸门🙏🏻”。纸鹿是一名开源爱好者，结识了许多志同道合的朋友。这个博客记录了他在生活和技术学习中的点滴经历，充满启发与思考。网站界面简洁美观，内容丰富实用，人气互动活跃，涵盖了编程、生活、学习等多个领域，为读者提供了卓越的阅读体验。',
+    description: 'GreenRoc的个人博客，分享技术与生活，框架直接从其他大佬那里fork过来的，网站下方有这位大佬的github仓库。',
     author: {
-        name: '纸鹿本鹿',
-        avatar: 'https://www.zhilu.cyou/api/avatar.png',
-        email: 'hi@zhilu.cyou',
-        homepage: 'https://www.zhilu.cyou/',
+        name: 'GreenRoc',
+        avatar: 'https://cd.ahhf45.top/img/IMG_0555.jpeg',
+        email: 'mollymathieson4@gmail.com',
+        homepage: 'https://ahhf45.top/',
     },
     copyright: {
         abbr: 'CC BY-NC-SA 4.0',
         name: '署名-非商业性使用-相同方式共享 4.0 国际',
         url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hans',
     },
-    favicon: 'https://www.zhilu.cyou/api/icon.png',
+    favicon: 'https://cd.ahhf45.top/img/IMG_0555.jpeg',
     language: 'zh-Hans',
-    qqGroup: '169994096',
-    timeEstablished: '2019-07-19',
+    timeEstablished: '2025-05-09',
     timezone: 'Asia/Shanghai',
-    url: 'https://blog.zhilu.cyou/',
+    url: 'https://ahhf45.top/',
 
     feed: {
         limit: 50,
@@ -48,9 +47,9 @@ const blogConfig = {
 
     scripts: [
         // 自己部署的 Umami 统计服务
-        { 'src': 'https://zhi.zhilu.cyou/zhi.js', 'data-website-id': 'a1997c81-a42b-46f6-8d1d-8fbd67a8ef41', 'defer': true },
+        { 'src': 'https://umami.ahhf45.top/script.js', 'data-website-id': '9b92a3f1-97bc-494c-aea9-436c5590421f', 'defer': true },
         // Cloudflare Insights 统计服务
-        { 'src': 'https://static.cloudflareinsights.com/beacon.min.js', 'data-cf-beacon': '{"token": "97a4fe32ed8240ac8284e9bffaf03962"}', 'defer': true },
+        //{ 'src': 'https://static.cloudflareinsights.com/beacon.min.js', 'data-cf-beacon': '{"token": "97a4fe32ed8240ac8284e9bffaf03962"}', 'defer': true },
     ],
 
     // 用于 Shiki、Plain Shiki 引入代码高亮
@@ -75,7 +74,7 @@ const blogConfig = {
 // 用于生成 OPML 和友链页面配置
 export const myFeed = <FeedEntry>{
     author: blogConfig.author.name,
-    sitenick: '摸鱼处',
+    sitenick: 'GreenRoc的博客',
     title: blogConfig.title,
     desc: blogConfig.subtitle || blogConfig.description,
     link: blogConfig.url,
