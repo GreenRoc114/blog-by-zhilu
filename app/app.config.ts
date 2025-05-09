3910847238d88c547c330bdda9061effc8b7cd12
GreenRoc114 <168276820+GreenRoc114@users.noreply.github.com>
2025-05-09 22:13:53 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -78,7 +78,7 @@ export default defineAppConfig({
 
     /** 左侧栏顶部 Logo */
     header: {
-        logo: 'https://weavatar.com/avatar/47c0f2e82b76d9b10eb3023df9e02e4e3fdbeaf5b74b842063f207971e7fbe7b?s=160',
+        logo: 'https://cd.ahhf45.top/img/IMG_0555.jpeg',
         /** 展示标题文本，否则展示纯 Logo */
         showTitle: true,
         subtitle: blogConfig.subtitle,
